31deee589458f3ad156274a97e3bd7f89d612014
datoprak <106121887+datoprak@users.noreply.github.com>
2023-10-31 23:33:30 +0300

    Change cardback img src for prod

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,9 @@
 import "../styles/Card.css";
+import img from "../assets/card-back.png";
 
 const Card = ({ poke, handleClick, flip }) => {
   const flipClass = flip ? "flip" : "";
-  const back = flip ? "" : "back"
+  const back = flip ? "" : "back";
 
   return (
     <div className="card" onClick={() => handleClick(poke.id)}>
@@ -12,17 +13,10 @@ const Card = ({ poke, handleClick, flip }) => {
           <div className="poke-name">{poke.name}</div>
         </div>
         <div className={back}>
-          <img src="./src/assets/card-back.png" alt="pokemon card back" className="card-back" />
+          <img src={img} alt="pokemon card back" className="card-back" />
         </div>
       </div>
     </div>
   );
 };
 export default Card;
-
-{
-  /* <div className="card" onClick={() => handleClick(poke.id)}>
-      <img src={poke.img} alt={poke.name} />
-      <div className="poke-name">{poke.name}</div>
-    </div> */
-}
